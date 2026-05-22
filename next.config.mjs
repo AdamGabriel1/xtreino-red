942a574532788a450371dfa99121ff700f21e2dd
Adam Gabriel <adamgabriel289@gmail.com>
2026-05-22 14:28:05 +0000

    Atualização

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').nextConfig}*/
-const nextConfig = {
-  reactStrictMode: true,
-  // Other config options here
-};
-
-export default nextConfig;
--- src/lib/defaultTeams.ts
+++
@@ -1,9 +0,0 @@
-export const defaultTeams = [
-  "RED MAGIC BR",
-  "RED DEVILS²",
-  "LÁ MAFIA",
-  "UGD THREAT",
-  "FURY RISING",
-  "UGD LIGHT",
-  "CMF ATLANTIC",
-]
